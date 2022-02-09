d5f22f46d6b0a8bc36ff1d12fe5bcbaa167c288a
Vishal Sharma <vishalsh94@yahoo.in>
2022-02-09 03:50:31 -0500

    NumCPU changed to 4, only 1 test
    Signed-off-by: Vishal Sharma <vishalsh94@yahoo.in>

--- launch_parsec_tests2.py
+++ launch_parsec_tests2.py
@@ -109,8 +109,8 @@ linux_binary = Artifact.registerArtifact(
 
 
 if __name__ == "__main__":
-    num_cpus = ['16']
-    benchmarks = ['blackscholes', 'bodytrack', 'canneal', 'dedup','facesim', 'ferret', 'fluidanimate', 'freqmine', 'raytrace', 'streamcluster', 'swaptions', 'vips', 'x264']
+    num_cpus = ['4'] #,'8','12','24']
+    benchmarks = ['blackscholes'] #, 'bodytrack', 'canneal', 'dedup','facesim', 'ferret', 'fluidanimate', 'freqmine', 'raytrace', 'streamcluster', 'swaptions', 'vips', 'x264']
 
     sizes = ['simsmall', 'simlarge', 'native']
     cpus = ['kvm', 'timing']
